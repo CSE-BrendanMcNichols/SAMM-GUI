31efda0392dad42bd87d26d31cb562a43a6ebf95
Mjw43 <144174555+Mjw43@users.noreply.github.com>
2024-04-15 22:59:10 -0400

    BackEnd fully integrated

--- demo/src/main/java/module-info.java
+++ demo/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.example {
     requires javafx.controls;
     requires javafx.fxml;
     requires json.simple;
+    requires com.google.gson;
 
     opens Code.backEnd to javafx.fxml;
     exports Code.backEnd;
