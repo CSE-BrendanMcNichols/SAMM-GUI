615214ee04be5d5274f04a866e3b1b945bfa1909
Mjw43 <144174555+Mjw43@users.noreply.github.com>
2024-04-16 12:46:30 -0400

    Test

---
+++ demo/src/main/java/com/example/advisorInfoController.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class advisorInfoController{
+
+    @FXML
+    private Button LogoutAdvisorInfo;
+
+}
---
+++ demo/src/main/java/com/example/loginController.java
@@ -0,0 +1,31 @@
+package com.example;
+
+import com.gluonhq.charm.glisten.mvc.View;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class loginController {
+
+    @FXML
+    private Button adminsitratorLoginButton;
+
+    @FXML
+    private Button advisorLoginButton;
+
+    @FXML
+    private View signinBackground;
+
+    @FXML
+    private Label signinText;
+
+    @FXML
+    private Label signinWelcome;
+
+    @FXML
+    private Button signupButton;
+
+    @FXML
+    private Button studentLoginButton;
+
+}
