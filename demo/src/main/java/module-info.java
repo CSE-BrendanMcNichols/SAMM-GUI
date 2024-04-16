3056251135e68a149f33fe4ba8d81eedf86328b5
Mjw43 <144174555+Mjw43@users.noreply.github.com>
2024-04-15 20:51:45 -0400

    Added basic css

--- demo/src/main/java/module-info.java
+++ demo/src/main/java/module-info.java
@@ -3,6 +3,9 @@ module com.example {
     requires javafx.fxml;
     requires json.simple;
 
+    opens Code.backEnd to javafx.fxml;
+    exports Code.backEnd;
+
     opens com.example to javafx.fxml;
     exports com.example;
 }
