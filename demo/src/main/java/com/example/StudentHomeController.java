ff0b7c5563dbc2121b10db241d02ad6a8093f8b6
CSE-BrendanMcNichols <mcnichob@email.sc.edu>
2024-04-20 22:25:42 -0400

    finished student home controller and added fxid to buttons in student home.fxml

---
+++ demo/src/main/java/com/example/StudentHomeController.java
@@ -0,0 +1,69 @@
+package com.example;
+
+import java.io.IOException;
+import Code.backEnd.*;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class StudentHomeController {
+
+    @FXML
+    private Label HelloName;
+
+    @FXML
+    private Button ViewInfo;
+
+    @FXML
+    private Button ViewSchedule;
+
+    @FXML
+    private Button ViewRoadmap;
+
+    @FXML
+    private Button TrackProgress;
+
+    @FXML
+    private Button ViewSendMessage;
+
+    @FXML
+    private Button Logout;
+
+    @FXML
+    private void viewInfo() throws IOException {
+        App.setRoot("studentInfo");
+    }
+
+    @FXML
+    private void viewSchedule() throws IOException {
+        App.setRoot("studentSchedule");
+    }
+
+    @FXML
+    private void viewRoadmap() throws IOException {
+        App.setRoot("studentRoadmap");
+    }
+
+    @FXML
+    private void trackProgress() throws IOException {
+        App.setRoot("studentProgress");
+    }
+
+    @FXML
+    private void viewSendMessage() throws IOException {
+        App.setRoot("studentMessages");
+    }
+
+    @FXML
+    private void logout() throws IOException {
+        Code.backEnd.ApplicationUI.saveData();
+        App.setRoot("login");
+    }
+
+    @FXML
+    private void initialize() {
+        ApplicationUI applicationUI = ApplicationUI.getInstance();
+
+        HelloName.setText("Hello, " + applicationUI.getStudent().getFirstName() + " " + applicationUI.getStudent().getLastName() + "!");
+    }
+}
